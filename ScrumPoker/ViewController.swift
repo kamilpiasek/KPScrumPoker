d92823bd82dcb86d9f40e47d4b3d31bb064226de
Kamil Piasek <kamil.piasek@homersoft.com>
2015-09-19 18:52:53 +0200

    Project start

---
+++ ScrumPoker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ScrumPoker
+//
+//  Created by Kamil Piasek on 19.09.2015.
+//  Copyright © 2015 Kamil Piasek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
